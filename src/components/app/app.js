4e9c01e678958aa376fa707a73d966a98bd7101c
Alexander Korobov <dktwho@mail.ru>
2023-03-05 14:52:57 +0300

    add component seach-panel with style

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,11 +2,15 @@
 import React from 'react'
 import './app.css'
 import AppInfo from '../app-info/app-info'
+import SearchPanel from '../search-panel/search-panel'
 
 const App = () => {
   return (
     <div className='app'>
       <AppInfo/>
+      <div className="search-panel">
+        <SearchPanel/>
+      </div>
 
     </div>
   )
