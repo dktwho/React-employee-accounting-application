9eff55bbd5f43af3b3b68c2978dfd5aa9ccc2fbc
Alexander Korobov <dktwho@mail.ru>
2023-03-05 14:48:38 +0300

    add component app-info with style

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import './app.css'
+import AppInfo from '../app-info/app-info'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <AppInfo/>
+
+    </div>
+  )
+}
+
+export default App
